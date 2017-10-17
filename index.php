e1e2ca9bdc28e0749af25297c6fcc33f2107ce46
thengboz <thengboz@gmail.com>
2017-10-17 21:36:36 +0000

    first commit

--- index.php
+++ index.php
@@ -1,67 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "thengboz";
+$password = "cst336";
+$dbname = "tech_devices_app"; 
 
-<!DOCTYPE html>
-<html>
-    
-<head>
-    <style>
-        @import url("css/styles.css");
-    </style>
-    
-    <title> 777 Slot Machine </title>
-</head>
-<body>
-    <?php
-        
-        function geturl($v){
-            
-            switch($v){
-                case 1:
-                    $imgurl= "./img/lemon.png";
-                    break;
-                case 2:
-                    $imgurl= "./img/cherry.png";
-                    break;
-                case 3:
-                    $imgurl= "./img/orange.png";
-                    break;
-                case 4:
-                    $imgurl= "./img/seven.png";
-                    break;
-            }
-            return $imgurl;
-        }
-        for ($i = 0; $i < 3; $i++){
-            $r[$i] = rand(1,4);
-        }
-        $url1 = geturl($r[0]);
-        $url2 = geturl($r[1]);
-        $url3 = geturl($r[2]);
-        echo "<div id='main'>";
-            echo "<img id='reel1' src=".$url1.">";
-            echo "<img id='reel2' src=".$url2.">";
-            echo "<img id='reel3' src=".$url3.">";
-            echo "<form action='./index.php'>";
-                echo "<input type='submit' value='spin!'/>";
-            echo "</form>";
-            echo "<div id='output'>";
-                $point = 0;
-                if($r[0]== $r[1] && $r[1] == $r[2]){
-                switch ($r[0]){
-                    case 1: $point = 250;
-                        break;
-                    case 2: $point = 500;
-                        break;
-                    case 4: $point = 1000;
-                        echo "<h1>Jackpot!!!</h1>";
-                        break;
-                }
-                echo "<h2>You won $point points</h2>";
-                }
-                else{
-                echo "<h3>Try Again!</h3>";
-                }
-            echo "</div>";
-        echo "</div>";
-    ?>
-</body>
-</html>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+ 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+// make a query 
+  
+$sql = "SELECT id, name, college FROM Department";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "department id: ".$row["id"] . ", name: ".$row["name"]. ", college: ".$row["college"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn -> close();
+?>
